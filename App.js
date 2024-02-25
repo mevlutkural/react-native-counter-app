810057972da229ebbe6bd2edf6d55e4224d85414
sam haze <mevlutkural4270@gmail.com>
2024-02-25 20:25:08 +0300

    most parts of project have been completed.

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+  const decrement = () => {
+    setCounter(counter - 1);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Counter App</Text>
+
+      <View style={styles.counterContainer}>
+        <Text style={{ fontSize: 40, textAlign: "center" }}>{counter}</Text>
+        <View
+          style={{ flex: 1, flexDirection: "row", justifyContent: "center" }}
+        >
+          <Button title="+" onPress={increment}  titleStyle={{marginHorizontal: 5}} />
+          <Button title="-" onPress={decrement} />
+        </View>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +34,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: 80,
+  },
+  title: {
+    fontSize: 40,
+    textAlign: "center",
+  },
+  counterContainer: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    marginTop: 300
   },
 });
